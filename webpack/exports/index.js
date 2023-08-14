983a3323d03a4434e9e6bcfa803fe3f12cded0eb
Scott Meesseman <spmeesseman@gmail.com>
2023-08-14 08:48:58 -0400

    build: finalization for wp build files (sync from te)<3it.should.a.been.me<3iwmbb<3broken4ever<3

--- webpack/exports/index.js
+++ webpack/exports/index.js
@@ -1,42 +1,33 @@
 // @ts-check
 
 import cache from "./cache";
-import context from "./context";
 import devtool from "./devtool";
 import entry from "./entry";
 import experiments from "./experiments";
 import externals from "./externals";
 import ignorewarnings from "./ignorewarnings";
 import minification from "./minification";
-import { mode, getMode } from "./mode";
-import name from "./name";
 import optimization from "./optimization";
 import plugins from "./plugins";
 import output from "./output";
 import resolve from "./resolve";
 import rules from "./rules";
 import stats from "./stats";
-import target from "./target";
 import watch from "./watch";
 
 export {
     cache,
-    context,
     devtool,
 	entry,
     experiments,
     externals,
-    getMode,
     ignorewarnings,
     minification,
-    mode,
-    name,
     optimization,
     plugins,
     output,
     resolve,
     rules,
     stats,
-    target,
     watch
 };
